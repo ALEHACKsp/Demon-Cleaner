3d8f6bda5cd371878ca0929b38515e071230f22c
ripped-oh <87819489+ripped-oh@users.noreply.github.com>
2021-08-19 09:10:03 -0700

    Create fishing-planet.bat

---
+++ Cleaners/fishing-planet.bat
@@ -0,0 +1,14 @@
+del /f %windir%\steam\steamapps\common\Fishing Planet\baselib.dll
+del /f %windir%\steam\steamapps\common\Fishing Planet\UnityPlayer.dll
+del /f %windir%\steam\steamapps\common\Fishing Planet\FishingPlanet_Data\boot.config
+
+del /f D:\steam\steamapps\common\Fishing Planet\baselib.dll
+del /f D:\steam\steamapps\common\Fishing Planet\UnityPlayer.dll
+
+del /f C:\steam\steamapps\common\Fishing Planet\baselib.dll
+del /f C:\steam\steamapps\common\Fishing Planet\UnityPlayer.dll
+
+del /f E:\steam\steamapps\common\Fishing Planet\baselib.dll
+del /f E:\steam\steamapps\common\Fishing Planet\UnityPlayer.dll
+
+exit
