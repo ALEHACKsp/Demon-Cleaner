c2d7d9289aed921056f308af5e58cdfbc1c1157c
ripped-oh <87819489+ripped-oh@users.noreply.github.com>
2021-08-22 05:09:28 -0700

    Add files via upload

---
+++ Cleaners/Fortnite.bat
@@ -0,0 +1,228 @@
+regdelete HKLM\\SOFTWARE\\WOW6432Node\\EasyAntiCheat /f
+regdelete HKLM\\SYSTEM\\ControlSet001\\Services\\EasyAntiCheat /f
+regdelete HKLM\\SYSTEM\\ControlSet001\\Services\\BEService /f
+rmdir /s /q C:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\CloudStore
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\FortniteGame\\Saved
+rmdir /s /q C:\\Windows\\INF
+rmdir /s /q C:\\ProgramData\\%username%\\Microsoft\\XboxLive\\NSALCache
+rmdir /s /q C:\\Users\\Public\\Documents
+rmdir /s /q C:\\Windows\\Prefetch
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\D3DSCache
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\CrashReportClient
+rmdir /s /q C:\\Windows\\temp
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\SettingSync\\metastore
+rmdir /s /q C:\\Windows\\SoftwareDistribution\\DataStore\\Logs
+rmdir /s /q C:\\ProgramData\\Microsoft\\Windows\\WER\\Temp
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\AMD\\DxCache
+rmdir /s /q \"C:\\Users\\%username%\\AppData\\Local\\NVIDIA Corporation
+@del /s /f /a:h / a : a / q C:\\Users\\username%\\AppData\\Local\\Packages\\Microsoft.Windows.Cortana_cw5n1h2txyewy\\*.*
+@del /s /f /a:h / a : a / q C:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\WebCache\\*.*
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\AC
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\LocalCache
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\Settings
+rmdir /s /q \"C:\\Program Files\\Epic Games\\Fortnite\\Engine\\Plugins
+rmdir /s /q \"C:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Plugins
+rmdir /s /q \"C:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\PersistentDownloadDir
+rmdir /s /q \"C:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Config
+rmdir /s /q C:\\Users\\%username%\\AppData\\Roaming\\EasyAntiCheat
+del /f /s /q C:\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\NetworksCache
+del /f /s /q C:\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\Rules
+rmdir /s /q C:\\Windows\\ServiceProfiles\\NetworkService\\AppData\\Local\\Microsoft\\Windows\\DeliveryOptimization\\Cache
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\Temp
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCache
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCookies
+rmdir /s /q C:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\History
+rmdir /s /q C:\\Users\\%username%\\Intel
+rmdir /s /q C:\\Windows\\System32\\config\\systemprofile\\AppData\\LocalLow\\Microsoft\\CryptnetUrlCache\\MetaData
+rmdir /s /q \"C:\\Users\\%username%\\AppData\\Local\\Microsoft\\Feeds Cache
+rmdir /s /q D:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\CloudStore
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\FortniteGame\\Saved
+rmdir /s /q D:\\Windows\\INF
+rmdir /s /q D:\\ProgramData\\%username%\\Microsoft\\XboxLive\\NSALCache
+rmdir /s /q D:\\Users\\Public\\Documents
+rmdir /s /q D:\\Windows\\Prefetch
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\D3DSCache
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\CrashReportClient
+rmdir /s /q D:\\Windows\\temp
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\SettingSync\\metastore
+rmdir /s /q D:\\Windows\\SoftwareDistribution\\DataStore\\Logs
+rmdir /s /q D:\\ProgramData\\Microsoft\\Windows\\WER\\Temp
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\AMD\\DxCache
+rmdir /s /q \"D:\\Users\\%username%\\AppData\\Local\\NVIDIA Corporation
+@del /s /f /a:h / a : a / q D:\\Users\\username%\\AppData\\Local\\Packages\\Microsoft.Windows.Cortana_cw5n1h2txyewy\\*.*
+@del /s /f /a:h / a : a / q D:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\WebCache\\*.*
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\AC
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\LocalCache
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\Settings
+rmdir /s /q \"D:\\Program Files\\Epic Games\\Fortnite\\Engine\\Plugins
+rmdir /s /q \"D:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Plugins
+rmdir /s /q \"D:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\PersistentDownloadDir
+rmdir /s /q \"D:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Config
+rmdir /s /q D:\\Users\\%username%\\AppData\\Roaming\\EasyAntiCheat
+del /f /s /q D:\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\NetworksCache
+del /f /s /q D:\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\Rules
+rmdir /s /q D:\\Windows\\ServiceProfiles\\NetworkService\\AppData\\Local\\Microsoft\\Windows\\DeliveryOptimization\\Cache
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\Temp
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCache
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCookies
+rmdir /s /q D:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\History
+rmdir /s /q D:\\Users\\%username%\\Intel
+rmdir /s /q D:\\Windows\\System32\\config\\systemprofile\\AppData\\LocalLow\\Microsoft\\CryptnetUrlCache\\MetaData
+rmdir /s /q \"D:\\Users\\%username%\\AppData\\Local\\Microsoft\\Feeds Cache
+rmdir /s /q E:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\CloudStore
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\FortniteGame\\Saved
+rmdir /s /q E:\\Windows\\INF
+rmdir /s /q E:\\ProgramData\\%username%\\Microsoft\\XboxLive\\NSALCache
+rmdir /s /q E:\\Users\\Public\\Documents
+rmdir /s /q E:\\Windows\\Prefetch
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\D3DSCache
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\CrashReportClient
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\SettingSync\\metastore
+rmdir /s /q E:\\Windows\\SoftwareDistribution\\DataStore\\Logs
+rmdir /s /q E:\\ProgramData\\Microsoft\\Windows\\WER\\Temp
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\AMD\\DxCache
+rmdir /s /q \"E:\\Users\\%username%\\AppData\\Local\\NVIDIA Corporation
+@del /s /f /a:h / a : a / q E:\\Users\\username%\\AppData\\Local\\Packages\\Microsoft.Windows.Cortana_cw5n1h2txyewy\\*.*
+@del /s /f /a:h / a : a / q E:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\WebCache\\*.*
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\AC
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\LocalCache
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\Settings
+rmdir /s /q \"E:\\Program Files\\Epic Games\\Fortnite\\Engine\\Plugins
+rmdir /s /q \"E:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Plugins
+rmdir /s /q \"E:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\PersistentDownloadDir
+rmdir /s /q \"E:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Config
+rmdir /s /q E:\\Users\\%username%\\AppData\\Roaming\\EasyAntiCheat
+del /f /s /q E:\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\NetworksCache
+del /f /s /q E:\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\Rules
+rmdir /s /q E:\\Windows\\ServiceProfiles\\NetworkService\\AppData\\Local\\Microsoft\\Windows\\DeliveryOptimization\\Cache
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\Temp
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCache
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCookies
+rmdir /s /q E:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\History
+rmdir /s /q E:\\Users\\%username%\\Intel
+rmdir /s /q E:\\Windows\\System32\\config\\systemprofile\\AppData\\LocalLow\\Microsoft\\CryptnetUrlCache\\MetaData
+rmdir /s /q \"E:\\Users\\%username%\\AppData\\Local\\Microsoft\\Feeds Cache
+rmdir /s /q F:\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\CloudStore
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\FortniteGame\\Saved
+rmdir /s /q F:\\Windows\\INF
+rmdir /s /q F:\\ProgramData\\%username%\\Microsoft\\XboxLive\\NSALCache
+rmdir /s /q F:\\Users\\Public\\Documents
+rmdir /s /q F:\\Windows\\Prefetch
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\D3DSCache
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\CrashReportClient
+rmdir /s /q F:\\Windows\\temp
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\SettingSync\\metastore
+rmdir /s /q F:\\Windows\\SoftwareDistribution\\DataStore\\Logs
+rmdir /s /q F:\\ProgramData\\Microsoft\\Windows\\WER\\Temp
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\AMD\\DxCache
+rmdir /s /q \"F:\\Users\\%username%\\AppData\\Local\\NVIDIA Corporation
+@del /s /f /a:h / a : a / q F:\\Users\\username%\\AppData\\Local\\Packages\\Microsoft.Windows.Cortana_cw5n1h2txyewy\\*.*
+@del /s /f /a:h / a : a / q F:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\WebCache\\*.*
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\AC
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\LocalCache
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\Settings
+rmdir /s /q \"F:\\Program Files\\Epic Games\\Fortnite\\Engine\\Plugins
+rmdir /s /q \"F:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Plugins
+rmdir /s /q \"F:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\PersistentDownloadDir
+rmdir /s /q \"F:\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Config
+rmdir /s /q F:\\Users\\%username%\\AppData\\Roaming\\EasyAntiCheat
+del /f /s /q F:\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\NetworksCache
+del /f /s /q F:\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\Rules
+rmdir /s /q F:\\Windows\\ServiceProfiles\\NetworkService\\AppData\\Local\\Microsoft\\Windows\\DeliveryOptimization\\Cache
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\Temp
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCache
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCookies
+rmdir /s /q F:\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\History
+rmdir /s /q F:\\Users\\%username%\\Intel
+rmdir /s /q F:\\Windows\\System32\\config\\systemprofile\\AppData\\LocalLow\\Microsoft\\CryptnetUrlCache\\MetaData
+rmdir /s /q \"F:\\Users\\%username%\\AppData\\Local\\Microsoft\\Feeds Cache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Temp
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Roaming\\EasyAntiCheat
+taskkill /f /im EpicGamesLauncher.exe
+taskkill /f /im FortniteClient-Win64-Shipping.exe
+taskkill /f /im OneDrive.exe
+rd /q /s %systemdrive%\\$Recycle.Bin
+rd /q /s d:\\$Recycle.Bin
+rd /q /s e:\\$Recycle.Bin
+rd /q /s f:\\$Recycle.Bin
+rmdir /s /q %systemdrive%\\Windows\\servicing\\InboxFodMetadataCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Roaming\\Microsoft\\Windows\\CloudStore
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\FortniteGame\\Saved
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\Explorer\\IconCacheToDelete
+rmdir /s /q %systemdrive%\\Windows\\INF
+rmdir /s /q %systemdrive%\\ProgramData\\%username%\\Microsoft\\XboxLive\\NSALCache
+rmdir /s /q %systemdrive%\\Windows\\Prefetch
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\D3DSCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\CrashReportClient
+rmdir /s /q %systemdrive%\\Windows\\temp
+rmdir /s /q %systemdrive%\\Windows\\Logs
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\SettingSync\\metastore
+rmdir /s /q %systemdrive%\\Windows\\SoftwareDistribution\\DataStore\\Logs
+rmdir /s /q %systemdrive%\\ProgramData\\Microsoft\\Windows\\WER\\Temp
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\AMD\\DxCache
+rmdir /s /q %systemdrive%\\ProgramData\\USOShared\\Logs
+@del /s /f /a:h / a : a / q %systemdrive%\\Users\\username%\\AppData\\Local\\Packages\\Microsoft.Windows.Cortana_cw5n1h2txyewy\\*.*
+@del /s /f /a:h / a : a / q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\WebCache\\*.*
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\AC
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\LocalCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.XboxGamingOverlay_8wekyb3d8bbwe\\Settings
+rmdir /s /q \"%systemdrive%\\Program Files\\Epic Games\\Fortnite\\Engine\\Plugins
+rmdir /s /q \"%systemdrive%\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Plugins
+rmdir /s /q \"%systemdrive%\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\PersistentDownloadDir
+rmdir /s /q \"%systemdrive%\\Users\\%username%\\AppData\\Local\\NVIDIA Corporation
+del /f /s /q %systemdrive%\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\NetworksCache
+del /f /s /q %systemdrive%\\ProgramData\\Microsoft\\DataMart\\PaidWiFi\\Rules
+rmdir /s /q %systemdrive%\\Windows\\ServiceProfiles\\NetworkService\\AppData\\Local\\Microsoft\\Windows\\DeliveryOptimization\\Cache
+rmdir / s / q %systemdrive%\\Users\\%username%\\AppData\\Local\\Temp
+rmdir /s /q %systemdrive%\\ProgramData\\%username%\\Microsoft\\XboxLive
+rmdir /s /q %systemdrive%\\Users\\Public\\Documents
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\NVIDIA Corporation
+@del /s /f /a:h / a : a / q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\XboxLive\\*.*
+rmdir /s /q \"%systemdrive%\\Program Files\\Epic Games\\Fortnite\\FortniteGame\\Config
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCookies
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\IEDownloadHistory
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\IECompatUaCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\IECompatCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCookies\\DNTException
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCookies\\PrivacIE
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\History
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\History\\Low
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.OneConnect_8wekyb3d8bbwe\\LocalState
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.MicrosoftOfficeHub_8wekyb3d8bbwe\\LocalCache\\EcsCache0
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.Windows.StartMenuExperienceHost_cw5n1h2txyewy\\TempState
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\TargetedContentCache\\v3
+rmdir /s /q %systemdrive%\\Users\\%username%\\Intel
+rmdir /s /q %systemdrive%\\Windows\\System32\\config\\systemprofile\\AppData\\LocalLow\\Microsoft\\CryptnetUrlCache\\MetaData
+rmdir /s /q \"%systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Feeds Cache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Feeds Cache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\EpicGamesLauncher
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\UnrealEngine
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\UnrealEngineLauncher
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\AMD
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\INTEL
+rmdir /s /q %systemdrive%\\Users\\%username%\\ntuser.ini
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\LocalLow\\Microsoft\\CryptnetUrlCache
+rmdir /s /q \"%systemdrive%\\System Volume Information\\IndexerVolumeGuid
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\CLR_v4.0
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\CLR_v3.0
+rmdir /s /q \"%systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Internet Explorer\\Recovery
+@del /s /f /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Feeds
+@del /s /f /q %systemdrive%\\Windows\\System32\\restore\\MachineGuid.txt
+@del /s /f /q %systemdrive%\\ProgramData\\Microsoft\\Windows\\WER
+@del /s /f /q %systemdrive%\\Users\\Public\\Libraries
+@del /s /f /q %systemdrive%\\MSOCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\WebCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\PowerShell\\StartupProfileData-NonInteractive
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\ConnectedDevicesPlatform\\L.%username%\\ActivitiesCache.db-wal
+rmdir /s /q %systemdrive%\\ProgramData\\USOShared\\Logs\\User
+@del /s /f /q %systemdrive%\\Users\\%username%\\AppData\\Local\\D3DSCache
+rmdir /s /q %systemdrive%\\Windows\\ServiceProfiles\\LocalService\\AppData\\Local\\ConnectedDevicesPlatform\\CDPGlobalSettings.cdp
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\cache\\qtshadercache
+@del /s /f /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\UsrClass.dat.log2
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\AMD\\VkCache
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\AMD\\CN\\NewsFeed
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\Windows\\INetCache\\IE\\RHKRUA8J
+rmdir /s /q %systemdrive%\\Users\\%username%\\AppData\\Local\\Microsoft\\CLR_v4.0\\UsageLogs
+rmdir /s /q %systemdrive%\\Windows\\Temp
+rmdir /s /q %systemdrive%\\Windows\\SERVIC~1\\NETWOR~1\\AppData\\Local\\Temp
