d635219ad7798d710649ca89545c8b9790588036
ripped-oh <87819489+ripped-oh@users.noreply.github.com>
2021-08-28 09:29:14 -0700

    Add files via upload

---
+++ DemonDLL/Demon DLL.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <windows.h>
+#include <Urlmon.h>
+#include <Wininet.h>
+#include <stdlib.h>
+#include <vector>
+#include <tchar.h>
+#include <iostream>
+
+#pragma comment(lib, "urlmon.lib")
+
+
+int main()
+{
+	int a = 20;
+	while (a = 20)
+	{
+		a * 2;
+	}
+}
